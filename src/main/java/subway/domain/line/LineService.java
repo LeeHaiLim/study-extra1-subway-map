3d83585c0812dd8c2cf1a8a3c93408b8b1dd37fd
Limjongho <whdgh9873@icloud.com>
2022-11-28 14:58:49 +0900

    refactor(LineService): 노선 생성 로직 변경

--- src/main/java/subway/domain/line/LineService.java
+++ src/main/java/subway/domain/line/LineService.java
@@ -1,19 +1,25 @@
 package subway.domain.line;
 
 import subway.domain.station.Station;
+import subway.domain.station.StationRepository;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class LineService {
     private final LineRepository lineRepository;
+    private final StationRepository stationRepository;
 
-    public LineService(LineRepository lineRepository) {
+    public LineService(LineRepository lineRepository, StationRepository stationRepository) {
         this.lineRepository = lineRepository;
+        this.stationRepository = stationRepository;
     }
 
-    public Line createLine(String lineName, List<Station> stations) {
+    public Line createLine(String lineName, List<String> stationNames) {
         isUniqueName(lineName);
+        isRegisteredStation(stationNames);
+        List<Station> stations = convertForm(stationNames);
         Line line = new Line(lineName, stations);
         return lineRepository.save(line);
     }
@@ -31,5 +37,20 @@ public class LineService {
         if (line.isPresent()) {
             throw new IllegalArgumentException("이미 존재하는 노선 이름입니다.");
         }
+    }
+
+    private void isRegisteredStation(List<String> stationNames) {
+        Optional<String> station = stationNames.stream()
+                .filter(stationName -> stationRepository.findByName(stationName).isEmpty())
+                .findAny();
+        if (station.isPresent()) {
+            throw new IllegalArgumentException("[ERROR] 등록되지 않은 역은 노선에 등록할 수 없습니다.");
+        }
+    }
+
+    private List<Station> convertForm(List<String> stationNames) {
+        return stationNames.stream()
+                .map(stationName -> stationRepository.findByName(stationName).get())
+                .collect(Collectors.toList());
     }
 }
