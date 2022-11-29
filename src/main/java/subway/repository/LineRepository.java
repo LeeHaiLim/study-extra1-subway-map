d475e72335edd094d068e46cdca5682df4875a24
EUNJEONGMUN <munej26@gmail.com>
2022-11-29 17:16:28 +0900

    feat: 존재하는 노선인지 확인하고, 추가하는 기능

--- src/main/java/subway/repository/LineRepository.java
+++ src/main/java/subway/repository/LineRepository.java
@@ -1,4 +1,7 @@
-package subway.domain;
+package subway.repository;
+
+import subway.domain.Line;
+import subway.domain.Station;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -13,7 +16,16 @@ public class LineRepository {
     }
 
     public static void addLine(Line line) {
+        if (isExistsLine(line)) {
+            throw new IllegalArgumentException("[ERROR] 이미 존재하는 노선입니다.");
+        }
         lines.add(line);
+    }
+    public static boolean isExistsLine(Line line) {
+        if (lines.contains(line)) {
+            return true;
+        }
+        return false;
     }
 
     public static boolean deleteLineByName(String name) {
