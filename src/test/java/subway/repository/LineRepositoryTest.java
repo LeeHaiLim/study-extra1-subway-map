98e94fdbc4f36330b7fcd32d12595d84b55af517
EUNJEONGMUN <munej26@gmail.com>
2022-12-01 20:52:12 +0900

    test: 노선에 속해있는 역인지 확인하는 테스트

--- src/test/java/subway/repository/LineRepositoryTest.java
+++ src/test/java/subway/repository/LineRepositoryTest.java
@@ -6,8 +6,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import subway.domain.Line;
 import subway.domain.LineName;
-
-import static org.junit.jupiter.api.Assertions.*;
+import subway.domain.Station;
+import subway.domain.StationName;
 
 class LineRepositoryTest {
     StationRepository stationRepository = StationRepository.getInstance();
@@ -37,5 +37,19 @@ class LineRepositoryTest {
         Assertions.assertThatThrownBy(() -> lineRepository.findLineByName(LineName.of("수인분당선")))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 존재하지 않는 노선입니다.");
+    }
+
+    @DisplayName("노선에 속해 있는 역이라면 true를 반환한다.")
+    @Test
+    void isStationBelongToLineTestTrue() {
+        Station 강남역 = stationRepository.findOrMakeStation(StationName.of("강남역"));
+        Assertions.assertThat(lineRepository.isStationBelongToLine(강남역)).isTrue();
+    }
+
+    @DisplayName("노선에 속해 있는 역이 아니면 false를 반환한다.")
+    @Test
+    void isStationBelongToLineTestFalse() {
+        Station 잠실역 = stationRepository.findOrMakeStation(StationName.of("잠실역"));
+        Assertions.assertThat(lineRepository.isStationBelongToLine(잠실역)).isFalse();
     }
 }
