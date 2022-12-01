6a6505da277abff1ee72f6d13fdf9f61ad63a89f
EUNJEONGMUN <munej26@gmail.com>
2022-12-01 14:39:03 +0900

    feat: 노선 등록 기능 구현

---
+++ src/main/java/subway/service/LineService.java
@@ -0,0 +1,21 @@
+package subway.service;
+
+import subway.domain.Line;
+import subway.domain.LineName;
+import subway.domain.Station;
+import subway.domain.StationName;
+import subway.repository.LineRepository;
+import subway.repository.StationRepository;
+
+import java.util.List;
+
+public class LineService {
+    private final StationRepository stationRepository = StationRepository.getInstance();
+    private final LineRepository lineRepository = LineRepository.getInstance();
+
+    public void addLine(LineName lineName, StationName firstStationName, StationName lastStationName) {
+        Station firstStation = stationRepository.findStationByName(firstStationName);
+        Station lastStation = stationRepository.findStationByName(lastStationName);
+        lineRepository.addLine(lineName, firstStation, lastStation);
+    }
+}
