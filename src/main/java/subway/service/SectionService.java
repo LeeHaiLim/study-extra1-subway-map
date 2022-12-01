58f48e165a68f7d6b598dded71b9e50b3d47fc21
EUNJEONGMUN <munej26@gmail.com>
2022-12-01 14:40:39 +0900

    feat: 구간 삭제 기능 구현

--- src/main/java/subway/service/SectionService.java
+++ src/main/java/subway/service/SectionService.java
@@ -14,4 +14,17 @@ public class SectionService {
         Station station = stationRepository.findStationByName(stationName);
         line.addStationToLineByOrder(order.getOrder(), station);
     }
+
+    public void deleteSection(LineName lineName, StationName stationName) {
+        Line line = lineRepository.findLineByName(lineName);
+        Station station = stationRepository.findStationByName(stationName);
+        checkLineContainOverTwoStation(line);
+        line.deleteStation(station);
+    }
+
+    private void checkLineContainOverTwoStation(Line line) {
+        if (line.numberOfStationsInLine() <= LIMIT_NUMBER_OF_STATION) {
+            throw new IllegalArgumentException("[ERROR] 노선에 포함된 역이 두개 이하일 때는 역을 제거할 수 없습니다.");
+        }
+    }
 }
